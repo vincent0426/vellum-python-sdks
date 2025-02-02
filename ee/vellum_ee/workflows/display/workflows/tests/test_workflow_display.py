ea7f307122d71ef20a031b5059b9180f5e31daf2
David Vargas <vargas@vellum.ai>
2025-02-01 23:31:46 -0500

    Confusing error when trying to add FinalOutputNode directly to graph (#172)

---
+++ ee/vellum_ee/workflows/display/workflows/tests/test_workflow_display.py
@@ -0,0 +1,37 @@
+import pytest
+
+from vellum.workflows.nodes.bases.base import BaseNode
+from vellum.workflows.workflows.base import BaseWorkflow
+from vellum_ee.workflows.display.workflows import VellumWorkflowDisplay
+from vellum_ee.workflows.display.workflows.get_vellum_workflow_display_class import get_workflow_display
+
+
+@pytest.mark.skip(reason="This test is not yet implemented")
+def test_serialize_workflow__node_referenced_in_workflow_outputs_not_in_graph():
+    # GIVEN a couple of nodes
+    class InNode(BaseNode):
+        pass
+
+    class OutNode(BaseNode):
+        class Outputs(BaseNode.Outputs):
+            foo: str
+
+    # AND A workflow that references the OutNode in its outputs but only has the InNode in its graph
+    class Workflow(BaseWorkflow):
+        graph = InNode
+
+        class Outputs(BaseWorkflow.Outputs):
+            final = OutNode.Outputs.foo
+
+    # WHEN we serialize it
+    workflow_display = get_workflow_display(
+        base_display_class=VellumWorkflowDisplay,
+        workflow_class=Workflow,
+    )
+
+    # THEN it should raise an error
+    with pytest.raises(ValueError) as exc_info:
+        workflow_display.serialize()
+
+    # AND the error message should be user friendly
+    assert str(exc_info.value) == "Failed to serialize output 'final': Reference to node 'OutNode' not found in graph."
